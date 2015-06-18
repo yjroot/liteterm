6d14f4fe4619fe1ac3507bdbf977e271bb9b9f8e
Yeonjun Lim <yjroot@gmail.com>
2015-06-18 14:00:32 +0900

    Initial Commit

---
+++ lightterm/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  lightterm
+//
+//  Created by yjroot on 2015. 6. 18..
+//  Copyright (c) 2015년 Netsarang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
