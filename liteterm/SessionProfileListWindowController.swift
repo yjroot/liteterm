c121094e38d8c4c7acf69149bffe80051f162901
Yeonjun Lim <yjroot@gmail.com>
2015-10-11 19:27:29 +0900

    Add session profile list window controller

---
+++ liteterm/SessionProfileListWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  SessionProfileListWindowController.swift
+//  liteterm
+//
+//  Created by yjroot on 2015. 10. 11..
+//  Copyright © 2015년 Liteterm Team. All rights reserved.
+//
+
+import Cocoa
+
+class SessionProfileListWindowController: NSWindowController {
+    convenience init() {
+        self.init(windowNibName: "SessionProfileListWindow")
+    }
+    
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    }
+}
